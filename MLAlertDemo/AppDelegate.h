d13217db4a8b9dd4ea5907aab7bfda0ff13d25d8
zhaoml <zml_xrzs@163.com>
2016-06-03 15:05:32 +0800

    Initial Commit

---
+++ MLAlertDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MLAlertDemo
+//
+//  Created by zhaoml on 16/6/3.
+//  Copyright © 2016年 zhaoml. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MLAlertDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MLAlertDemo
+//
+//  Created by zhaoml on 16/6/3.
+//  Copyright © 2016年 zhaoml. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
